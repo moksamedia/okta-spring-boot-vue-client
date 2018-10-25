2c5ced9e51b832e70f485362fcc15dd01dc2bd13
Andrew Hughes <andrewcarterhughes@gmail.com>
2018-10-24 18:15:12 -0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,221 @@
+<template>
+  <div id="app">
+    <section class="todoapp">
+      <header class="header">
+        <h1>todos</h1>
+        <input class="new-todo"
+               autofocus autocomplete="off"
+               placeholder="What needs to be done?"
+               v-model="newTodo"
+               @keyup.enter="addTodo">
+      </header>
+      <section class="main" v-show="todos.length" v-cloak>
+        <input class="toggle-all" type="checkbox" v-model="allDone">
+        <ul class="todo-list">
+          <li v-for="todo in filteredTodos"
+              class="todo"
+              :key="todo.id"
+              :class="{ completed: todo.completed, editing: todo == editedTodo }">
+            <div class="view">
+              <input class="toggle" type="checkbox" v-model="todo.completed">
+              <label @dblclick="editTodo(todo)">{{ todo.title }}</label>
+              <button class="destroy" @click="removeTodo(todo)"></button>
+            </div>
+            <input class="edit" type="text"
+                   v-model="todo.title"
+                   v-todo-focus="todo == editedTodo"
+                   @blur="doneEdit(todo)"
+                   @keyup.enter="doneEdit(todo)"
+                   @keyup.esc="cancelEdit(todo)">
+          </li>
+        </ul>
+      </section>
+      <footer class="footer" v-show="todos.length" v-cloak>
+      <span class="todo-count">
+        <strong>{{ remaining }}</strong> {{ remaining | pluralize }} left
+      </span>
+        <ul class="filters">
+          <li><a href="#/all" @click="setVisibility('all')" :class="{ selected: visibility == 'all' }">All</a></li>
+          <li><a href="#/active" @click="setVisibility('active')" :class="{ selected: visibility == 'active' }">Active</a></li>
+          <li><a href="#/completed" @click="setVisibility('completed')" :class="{ selected: visibility == 'completed' }">Completed</a></li>
+        </ul>
+        <button class="clear-completed" @click="removeCompleted" v-show="todos.length > remaining">
+          Clear completed
+        </button>
+      </footer>
+    </section>
+    <footer class="info">
+      <p>Based on a project written by <a href="http://evanyou.me">Evan You</a></p>
+      <p>Originally part of <a href="http://todomvc.com">TodoMVC</a></p>
+      <p>Modified for Okta for Andrew Hughes</p>
+    </footer>
+  </div>
+</template>
+
+<script>
+
+  // Full spec-compliant TodoMVC with localStorage persistence
+  // and hash-based routing in ~120 effective lines of JavaScript.
+
+  // localStorage persistence
+  let STORAGE_KEY = 'todos-vuejs-2.0'
+  let todoStorage = {
+    fetch: function () {
+      var todos = JSON.parse(localStorage.getItem(STORAGE_KEY) || '[]')
+      todos.forEach(function (todo, index) {
+        todo.id = index
+      })
+      todoStorage.uid = todos.length
+      return todos
+    },
+    save: function (todos) {
+      localStorage.setItem(STORAGE_KEY, JSON.stringify(todos))
+    }
+  }
+
+  // visibility filters
+  let filters = {
+    all: function (todos) {
+      return todos
+    },
+    active: function (todos) {
+      return todos.filter(function (todo) {
+        return !todo.completed
+      })
+    },
+    completed: function (todos) {
+      return todos.filter(function (todo) {
+        return todo.completed
+      })
+    }
+  }
+
+  // app Vue instance
+  const app = {
+
+    name: 'app',
+
+    // app initial state
+    data: () => {
+      return {
+        todos: todoStorage.fetch(),
+        newTodo: '',
+        editedTodo: null,
+        visibility: 'all'
+      }
+    },
+
+    // watch todos change for localStorage persistence
+    watch: {
+      todos: {
+        handler: function (todos) {
+          todoStorage.save(todos)
+        },
+        deep: true
+      },
+      '$route': {
+        handler: (to) => {
+          alert(to)
+        }
+      }
+
+    },
+
+    // computed properties
+    // http://vuejs.org/guide/computed.html
+    computed: {
+      filteredTodos: function () {
+        return filters[this.visibility](this.todos)
+      },
+      remaining: function () {
+        return filters.active(this.todos).length
+      },
+      allDone: {
+        get: function () {
+          return this.remaining === 0
+        },
+        set: function (value) {
+          this.todos.forEach(function (todo) {
+            todo.completed = value
+          })
+        }
+      }
+    },
+
+    filters: {
+      pluralize: function (n) {
+        return n === 1 ? 'item' : 'items'
+      }
+    },
+
+    // methods that implement data logic.
+    // note there's no DOM manipulation here at all.
+    methods: {
+      addTodo: function () {
+        var value = this.newTodo && this.newTodo.trim()
+        if (!value) {
+          return
+        }
+        this.todos.push({
+          id: todoStorage.uid++,
+          title: value,
+          completed: false
+        })
+        this.newTodo = ''
+      },
+
+      setVisibility: function(vis) {
+        this.visibility = vis
+      },
+
+      removeTodo: function (todo) {
+        this.todos.splice(this.todos.indexOf(todo), 1)
+      },
+
+      editTodo: function (todo) {
+        this.beforeEditCache = todo.title
+        this.editedTodo = todo
+      },
+
+      doneEdit: function (todo) {
+        if (!this.editedTodo) {
+          return
+        }
+        this.editedTodo = null
+        todo.title = todo.title.trim()
+        if (!todo.title) {
+          this.removeTodo(todo)
+        }
+      },
+
+      cancelEdit: function (todo) {
+        this.editedTodo = null
+        todo.title = this.beforeEditCache
+      },
+
+      removeCompleted: function () {
+        this.todos = filters.active(this.todos)
+      }
+    },
+
+    // a custom directive to wait for the DOM to be updated
+    // before focusing on the input field.
+    // http://vuejs.org/guide/custom-directive.html
+    directives: {
+      'todo-focus': function (el, binding) {
+        if (binding.value) {
+          el.focus()
+        }
+      }
+    }
+  }
+
+  export default app
+
+</script>
+
+<style>
+
+[v-cloak] { display: none; }
+
+</style>
